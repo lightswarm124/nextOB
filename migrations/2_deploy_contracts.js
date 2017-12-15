88c17ca53db3040435f27aca20cb721076fc168b
lightswarm124 <lightswarm@gmail.com>
2017-12-14 21:34:43 -0500

    began porting LandingPage.js

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -1,5 +1,12 @@
-const SimpleStorage = artifacts.require('./SimpleStorage.sol')
-
+const SimpleStorage = artifacts.require('./SimpleStorage.sol');
+/*
+const ConvertLib = artifacts.require('./ConvertLib.sol');
+const MetaCoin = artifacts.require('./MetaCoin.sol');
+*/
 module.exports = function (deployer) {
-  deployer.deploy(SimpleStorage)
+  deployer.deploy(SimpleStorage);
+  /*deployer.deploy(ConvertLib);
+  deployer.link(SimpleStorage, MetaCoin);
+  deployer.deploy(SimpleStorage);
+  */
 }
