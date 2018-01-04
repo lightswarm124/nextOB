8f2035c29d02a25e3d14727e5b1d884bc4868397
lightswarm124 <lightswarm@gmail.com>
2018-01-03 19:54:18 -0500

    test

---
+++ test/OpenBountyTest.sol
@@ -0,0 +1,12 @@
+pragma solidity ^0.4.18;
+
+import "truffle/Assert.sol";
+import "truffle/DeployedAddresses.sol";
+import "../contracts/WIP/OpenBounty.sol";
+
+contract OpenBountyTest {
+    function testInitialize() public {
+    
+    }
+}
+
