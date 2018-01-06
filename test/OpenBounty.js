2c7a0d9c7edfc9fce223701fd4ed3a6c09470ebd
lightswarm124 <lightswarm@gmail.com>
2018-01-06 11:04:16 -0500

    OB tests

--- test/OpenBounty.js
+++ test/OpenBounty.js
@@ -1,16 +1,23 @@
 const OpenBounty = artifacts.require('./WIP/OpenBounty.sol');
 
 contract('Test OpenBounty Initialization', (accounts) => {
-    it('Test initialization', async () => {
-        let testOB;
+    it('Test address initialization', async () => {
         let ownerAddress;
-        
-        return OpenBounty.new().then((instance) => {
-            testOB = instance;
-            return testOB.ProjectOwner;
-        }).then((array) => {        
-            ownerAddress = array.toString();
-            assert.equal(ownerAddress, accounts[0], "Check deployment address");
-        }); 
+
+        OpenBounty.new({from: accounts[0]})
+        .then((inst) => {
+            return inst.bounty.ProjectOwner;
+        }).then((addr) => {
+            ownerAddress = addr;
+            assert.equal(ownerAddress, accounts[0], 'Contract deployer is not set to ProjectOwner'); 
+        });
+        /*
+        OpenBounty.deployed()
+        .then((inst) => {
+            return inst.bounty.ProjectManagers[ownerAddress];
+        }).then((isManager) => {
+            assert.equal(isManager, true, 'Contract deployer is not set to ProjectManagers');
+        });
+        */
     });
 });  
