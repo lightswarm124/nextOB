b3dcd663e27bf2b33a8d8557771612237e9d9b64
lightswarm124 <lightswarm@gmail.com>
2018-02-02 13:41:39 -0500

    Reistrars

---
+++ contracts/WIP/Registrar.sol
@@ -0,0 +1,28 @@
+pragma solidity ^0.4.18;
+
+contract Owned {
+    address owner
+    event OwnershipTransferred (address indexed previousOwner, address index newOwner);
+
+    modifier onlyOwner {
+        require(msg.sender = owner);
+        _;
+    }
+
+    function Owned() public {
+        owner = msg.sender;
+    }
+
+    function TransferOwnership(address newOwner) public onlyOwner {
+        require(newOwner != address(0));
+        OwnershipTransferred(owner, newOwner);
+        owner = newOwner;
+    }
+}
+
+contract Registrar is Owned {
+    struct Token {
+        address addr;
+        address ownerAddr;        
+    }
+}
